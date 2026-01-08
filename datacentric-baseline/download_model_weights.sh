2cfdea5524165d39b88faee025b6f028e1a95aec
jdex <38916914+JakobDexl@users.noreply.github.com>
2026-01-08 13:46:26 +0100

    Refactor download_model_weights.sh for GitHub release
    Updated the script to download model weights from a GitHub release instead of multiple URLs.

--- datacentric-baseline/download_model_weights.sh
+++ datacentric-baseline/download_model_weights.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+set -e
 
-declare -a arr_data=(
-  "fiQpSvvDinQGwCixsZmh1k/config.yml"
-  "fiDMtokM94d6LPgyxAixzx/last.ckpt"
-  "fiNi5TKUvZ6GJVwe3WqhZY/events.out.tfevents.all_samples.0"
-  )
+TAG="final"
+ZIP_NAME="AutoPETIII_datacentric_baseline.zip"
 
-for path in "${arr_data[@]}"; do
-    file=$(basename "$path")
-    echo "Download: ${file}"
-    curl --create-dirs -o "weights/${file}" "https://syncandshare.lrz.de/dl/${path// /%20}"
-done
+URL="https://github.com/ClinicalDataScience/autoPETIII/releases/download/${TAG}/${ZIP_NAME}"
 
+mkdir -p weights
+tmpdir=$(mktemp -d)
+
+echo "Download: ${ZIP_NAME}"
+curl -L -o "${tmpdir}/${ZIP_NAME}" "${URL}"
+
+echo "Extracting files"
+unzip -j "${tmpdir}/${ZIP_NAME}" \
+  config.yml \
+  last.ckpt \
+  events.out.tfevents.all_samples.0 \
+  -d weights
+
+rm -rf "${tmpdir}"
